ae4c238e26292e13f8d9934da80ea378610ff912
Stefan <lazeastefan@gmail.com>
2020-02-20 13:24:51 +0200

    First db schema

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -3,14 +3,23 @@ const User = sequelize.import('./user');
 const Car = sequelize.import('./car');
 const Garage = sequelize.import('./garage');
 const garagecars = sequelize.import('./garageCars');
+const carpapers = sequelize.import('./carPapers');
+const Paper = sequelize.import('./paper');
 
 Car.belongsToMany(Garage, { through: 'garagecars' });
 Garage.belongsToMany(Car, { through: 'garagecars' });
+
+User.hasMany(Car);
+
+Car.belongsToMany(Paper, { through: 'carpapers' });
+Paper.belongsToMany(Car, { through: 'carpapers' });
+
 
 module.exports = {
     User,
     Car,
     Garage,
     garagecars,
+    Paper,
     sequelize
 }
---
+++ back-end/models/paper.js
@@ -0,0 +1,18 @@
+let Sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('papers',
+        {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV1,
+                primaryKey: true
+            },
+            type: Sequelize.STRING,
+            details: Sequelize.STRING
+        },
+        {
+            underscored: true
+        }
+    );
+}
