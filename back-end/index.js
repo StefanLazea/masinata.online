d6b38a72aec271e966d45b1ef7497a3dcccdc0be
Stefan <lazeastefan@gmail.com>
2020-03-06 01:30:38 +0200

    Cors update

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser(COOKIE_SECRET));
 
-app.use(cors());
+
+// app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "DELETE,PUT");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 // model.sequelize.sync({ force: true });
 model.sequelize.sync();
