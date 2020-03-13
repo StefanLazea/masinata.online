3c1f40c37d78ebb278ad1bfc2bd47be363b6150c
Stefan <lazeastefan@gmail.com>
2020-03-14 00:33:22 +0200

    Add redirect for signup and signin buttons

--- front-end/src/components/Landing/Landing.js
+++ front-end/src/components/Landing/Landing.js
@@ -9,10 +9,18 @@ export default class Landing extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            redirectToSignUp: false
+            redirectToSignUp: false,
+            redirectToLogin: false,
         };
     }
     render() {
+        console.log(this.state.redirectToLogin, this.state.redirectToSignUp);
+        if (this.state.redirectToSignUp) {
+            return <Redirect to="/signup" />;
+        }
+        if (this.state.redirectToLogin) {
+            return <Redirect to="/login" />;
+        }
         return (
             <div className="row">
                 <div className="col-xs-6 col-sm-6 col-md-6">
@@ -22,8 +30,8 @@ export default class Landing extends React.Component {
                     <h1 id="brand" className="center-text">masinaTa.online</h1>
                     <h6 id="slogan" className="center-text">Forget about your car</h6>
                     <div className="row-3">
-                        <Button id="button">Sign up</Button>
-                        <Button id="second">Sign in</Button>
+                        <Button id="button" onClick={() => { this.setState({ redirectToSignUp: true }) }}>Sign up</Button>
+                        <Button id="second" onClick={() => { this.setState({ redirectToLogin: true }) }}>Sign in</Button>
                     </div>
 
                 </div>
