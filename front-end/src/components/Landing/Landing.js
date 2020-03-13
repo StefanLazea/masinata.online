8f8c9ec43b5f52ebfec430086a193052e28dab37
Stefan <lazeastefan@gmail.com>
2020-03-13 23:10:52 +0200

    Change text orientation on full screen

--- front-end/src/components/Landing/Landing.js
+++ front-end/src/components/Landing/Landing.js
@@ -12,8 +12,8 @@ export default class Landing extends React.Component {
                     <img id="img" src={car} alt="car"></img>
                 </div>
                 <div id="text" className="col-xs-6 col-sm-6 col-md-6">
-                    <h1 className="center-text">masinaTa.online</h1>
-                    <h6 className="center-text">Forget about your car</h6>
+                    <h1 id="brand" className="center-text">masinaTa.online</h1>
+                    <h6 id="slogan" className="center-text">Forget about your car</h6>
                     <Button id="button">Press me</Button>
                 </div>
             </div>
