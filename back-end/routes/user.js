4214093da8a0cd607b746a628f004fadb57ff38a
Stefan <lazeastefan@gmail.com>
2020-03-17 00:22:47 +0200

    Add get for users details

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -3,4 +3,24 @@ const User = require("../models").User;
 const { authorize } = require("../services/authorize");
 const Role = require("../helpers/role");
 
+router.get("/details/:id", authorize([Role.User, Role.Admin, Role.AppAdmin]), async (req, res) => {
+    let userDetails = await User.findOne({
+        attributes: [
+            "role",
+            "lastname",
+            "firstname",
+            "address",
+            "phone"
+        ], where: {
+            id: req.params.id
+        }
+    });
+
+    if (userDetails != null) {
+        return res.status(200).send(userDetails);
+    }
+    return res.status(400).send({ "message": "Data unclear" })
+
+});
+
 module.exports = router;
